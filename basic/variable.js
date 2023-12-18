f03cdf489662a93fe14246381e9bb8a3c092a3e3
Varad Naik <120413511+VARADNAIK024@users.noreply.github.com>
2023-12-18 10:13:48 +0000

    container config

---
+++ basic/variable.js
@@ -0,0 +1,12 @@
+ const accountID = 123456;
+let accountEmail = "varu@gmail.com";
+let accountPass = "varu";
+let accountCity = "satara";
+
+ // accountID = 23345 CONST VALUES CANNT BE CHANGE IN CODE :)
+
+console.log(accountID);//FOR SINGLE VARIABLE OUTPUT
+accountPass = "vara@024"
+
+
+console.table([accountID,accountEmail,accountPass,accountCity,]); //FOR MULTIPLE VARIABLE OUTPUT
