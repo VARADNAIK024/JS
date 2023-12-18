13884129d5780b87661a033755cc6e1a3329c961
Varad Naik <120413511+VARADNAIK024@users.noreply.github.com>
2023-12-18 14:46:30 +0000

    conditional operation

--- basic/conoperation.js
+++ basic/conoperation.js
@@ -4,7 +4,7 @@ let score = "33";
 // console.log(typeof(score));
 
 let value = Number(score);
-console.log(typeof value);
+//console.log(typeof value);
 
 // 33 = 33 -> can be converted 
 // "33abc" = NaN -> Not a Number 
@@ -12,14 +12,30 @@ console.log(typeof value);
 
 let isLoggedIn = 1;
 let booleenislogged = Boolean(isLoggedIn);
-console.log(booleenislogged);
+//console.log(booleenislogged);
 
 
 let login = 0;
 let valid = Boolean(login);
-console.log(valid);
+//console.log(valid);
 
 // 1 -> true
 // 0 -> false
 // " "  ->  false  -> undefined
 // "varad" ->  true -> any String
+
+/-------------------------------------------------------------/
+
+// To Combine 2 STR
+let str1 = "varu";
+let str2 = " naik";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+//OPERATIONS ON JS +,-,*,/,%
+console.log(2+2);
+console.log(2-1);
+console.log(2*2);
+console.log(2/2);
+console.log(22%3);
