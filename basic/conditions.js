fefe1d01ce5f035f961023daa09028737e8e27c4
Varad Naik <120413511+VARADNAIK024@users.noreply.github.com>
2023-12-18 17:43:21 +0000

    conditions in js

---
+++ basic/conditions.js
@@ -0,0 +1,12 @@
+console.log(2 < null);
+
+//IN NULL ONLY ONE CONDITION IS TRUE
+console.log(null == 0);
+console.log(null> 0);
+console.log(null >= 0); // Null is Greater and Equal to Zero
+
+
+//IN UNDEFEINED ALL CONFITIONS ARE FALSE
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined <  0);
