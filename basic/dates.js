d377b1f28a6f5a582bee4ee08ab9fbe1115f86bc
Varad Naik <120413511+VARADNAIK024@users.noreply.github.com>
2023-12-21 08:42:11 +0000

    Dates in JS

---
+++ basic/dates.js
@@ -0,0 +1,13 @@
+// DATE IS AN OBJECT IN JS
+
+// const mydate = new Date()
+// console.log(mydate.toDateString());
+// console.log(mydate.toString());
+// console.log(mydate.toJSON())//2023-12-21T08:21:25.504Z
+
+// let date1 = new Date(2002, 11 , 25)
+// console.log(date1.toString());
+
+let Mytime =  Date.now()
+console.log(Mytime);
+
