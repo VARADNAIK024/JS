5e59383c3452bdc15109075cf8845f7742efbb03
Varad Naik <120413511+VARADNAIK024@users.noreply.github.com>
2023-12-29 18:35:25 +0000

    SWITCH CASE ( )

---
+++ basic4/switch.js
@@ -0,0 +1,49 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+
+const order = [1,2,3,4,5];
+//const order1 = 3;
+switch (order) {
+    case 1:
+        console.log("pizza");
+        break;
+        case 2:
+            console.log("burger");
+            break;
+            case 3:
+                console.log("pasta");
+                break;
+                
+
+    default:
+        console.log("normal mill")
+        break;
+}
