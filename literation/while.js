642dbf43383cfbffd0d042aa77c7573bbe31b439
Varad Naik <120413511+VARADNAIK024@users.noreply.github.com>
2023-12-29 20:14:57 +0000

    While Loop ( )

---
+++ literation/while.js
@@ -0,0 +1,36 @@
+// //SYNTAX OF WJILE LOOP
+
+// // while (condition) {
+    
+// // }
+
+
+// let score =  1;
+// while (score <= 10) {
+//    // console.log(score);
+//   //  score ++;
+    
+// }
+
+// // Printing array using while loop 
+
+// let myarr = ["flash","iron","thor","siperman"]
+// let index = 0;
+// while (index < myarr.length) 
+// {
+//  //console.log(myarr);
+// // console.log(`values are ${myarr[index]}`);
+//  //index++ ;   
+ 
+// }
+
+
+// // DO WHILE LOOP
+
+
+
+let value = 1;
+do {
+    console.log(`value ${value}`);
+    value++
+} while (value <=10);
