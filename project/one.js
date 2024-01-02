8808b6442c0b85aa4ba7928bf9e5cef0e9ea1c46
Varad Naik <120413511+VARADNAIK024@users.noreply.github.com>
2024-01-02 19:46:44 +0000

    color clicking

---
+++ project/one.js
@@ -0,0 +1,33 @@
+const buttons = document.querySelectorAll('.button');
+//console.log(buttons)
+const body = document.querySelector('body');
+//console.log(body)
+
+// buttons.forEach(function(button){
+//   console.log(button);
+//   button.addEventListener('click',(e) => {
+//     console.log(e)
+//     console.log(e.target)
+
+//   })
+// })
+
+  buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    //console.log(e);
+    //console.log(e.target);
+    if(e.target.id === 'grey'){
+    body.style.backgroundColor = e.target.id
+    }
+    if(e.target.id === 'red'){
+      body.style.backgroundColor = 'red'
+    }
+    if(e.target.id === 'blue'){
+      body.style.backgroundColor ="blue"
+    }
+    if(e.target.id ==="yellow"){
+      body.style.backgroundColor = "yellow"
+    }
+  });
+});
+
